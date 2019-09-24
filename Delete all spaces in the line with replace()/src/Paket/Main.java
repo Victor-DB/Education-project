86189bf7f0a427a5274c44698d60ffa8245ba752
Victor-DB <52582717+Victor-DB@users.noreply.github.com>
2019-09-24 16:27:58 +0300

    transfer to github

---
+++ Delete all spaces in the line with replace()/src/Paket/Main.java
@@ -0,0 +1,11 @@
+package Paket;
+
+// Delete all spaces in the line with replace()
+public class Main {
+    public static void main (String[] args) {
+        String s = "Put your hands up in the air";
+        String sNew = s.replace(" ", "");
+        System.out.println(sNew);
+    }
+}
+
